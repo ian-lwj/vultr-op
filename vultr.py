988719c83fd76d4acb615c97bb7d6d73d77dff54
liuwenjun <184688210@qq.com>
2019-01-14 10:28:53 +0800

    add default sshkeyid

--- vultr.py
+++ vultr.py
@@ -6,7 +6,8 @@ import socket
 import sys
 
 
-APIKey = 
+APIKey = None
+SSHKEYID = None
 
 
 class Vultr:
@@ -23,20 +24,33 @@ class Vultr:
             headers=Vultr.headers, data=payload)
         print r.status_code, r.content
 
-    def create_vm(self):
+    def create_vm(self, sshkeyid=SSHKEYID):
         payload = {'DCID': '5', 'VPSPLANID': '200', 'OSID': 302}
+        if sshkeyid is not None:
+            payload['SSHKEYID'] = sshkeyid
         r = requests.post('https://api.vultr.com/v1/server/create',
             headers=Vultr.headers, data=payload, timeout=9)
         print r.status_code, r.content
 
-    def add_ssh_key(self):
+    def create_ssh_key(self):
         home = expanduser("~")
         with open(home + "/.ssh/id_rsa.pub", "r") as f:
             key = f.read()
             payload = {'name': socket.gethostname(), 'ssh_key': key}
             r = requests.post('https://api.vultr.com/v1/sshkey/create',
-                    header=Vultr.headers, data=payload)
+                    headers=Vultr.headers, data=payload)
             print r.status_code, r.content
+
+    def list_ssh_key(self):
+        r = requests.get('https://api.vultr.com/v1/sshkey/list',
+                          headers=Vultr.headers)
+        print r.status_code, r.content
+
+    def destroy_ssh_key(self, sshkeyid):
+        payload = {'SSHKEYID': sshkeyid}
+        r = requests.post('https://api.vultr.com/v1/sshkey/destroy',
+                          headers=Vultr.headers, data=payload, timeout=9)
+        print r.status_code, r.content
 
     
 if __name__ == '__main__':
@@ -45,10 +59,11 @@ if __name__ == '__main__':
     parser_vm = subparsers.add_parser('vm', help='vm opertation.')
     parser_vm.add_argument('operation', choices=['list', 'create', 'destroy'])
     parser_vm.add_argument('subid', nargs='?', help="SUBID for destroy.")
+    parser_vm.add_argument('--sshkeyid', help="SSHKEYID for create.")
 
     parser_sshkey = subparsers.add_parser('sshkey', help='sshkey operation.')
-    parser_sshkey.add_argument('operation', choices=['list', 'add', 'delete'])
-    parser_sshkey.add_argument('sshkeyid', nargs='?', help='SSHKEYID for delete.')
+    parser_sshkey.add_argument('operation', choices=['list', 'create', 'destroy'])
+    parser_sshkey.add_argument('sshkeyid', nargs='?', help='SSHKEYID for destroy.')
 
     args = parser.parse_args()
 
@@ -57,15 +72,17 @@ if __name__ == '__main__':
         if args.operation == 'list':
             vul.list_vms()
         elif args.operation == 'create':
-            vul.create_vm()
+            sshkeyid = args.sshkeyid
+            vul.create_vm(sshkeyid)
         elif args.operation == 'destroy':
             subid = args.subid
             vul.destroy_vm(subid)
     elif args.obj == 'sshkey':
         if args.operation == 'list':
-            pass
-        elif args.operation == 'add':
-            vul.add_sshkey()
-        elif args.operation == 'delete':
-            pass
+            vul.list_ssh_key()
+        elif args.operation == 'create':
+            vul.create_sshkey(sshkeyid)
+        elif args.operation == 'destroy':
+            sshkeyid = args.sshkeyid
+            vul.destroy_ssh_key(sshkeyid)
 
